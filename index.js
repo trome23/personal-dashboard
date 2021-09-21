9379820805a337a9c265d370575883aa9179a98e
Tyson Romero <romennoodles23@yahoo.com>
2021-09-21 16:52:28 -0600

    getting random image from API and added flexbox layout

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-fetch('https://api.unsplash.com/photos/random?client_id=S1b4bEGNT4tRS7aoerHUBbn7USA2bUiSHZ475KDWtlc')
+fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=S1b4bEGNT4tRS7aoerHUBbn7USA2bUiSHZ475KDWtlc')
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.querySelector('#author-name').textContent = `By: ${data.user.name}`
     })
