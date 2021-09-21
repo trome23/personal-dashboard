3cdb43db8e4e55bd2deaea27698d56a3c5c2e00f
Tyson Romero <romennoodles23@yahoo.com>
2021-09-21 17:07:58 -0600

    just added catch in fetch request

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=S1b4bEGNT4tRS7aoerHUBbn7USA2bUiSHZ475KDWtlc')
     .then(res => res.json())
     .then(data => {
+        console.log(data.urls.full);
         document.body.style.backgroundImage = `url(${data.urls.full})`
         document.querySelector('#author-name').textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+        document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjIwNTV8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzIyNjU1NDU&ixlib=rb-1.2.1&q=85')`
+        document.querySelector('#author-name').textContent = "By: Jay Mantri"
     })
