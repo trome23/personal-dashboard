465b1d920fd7496102b1346610bb2e5a67b41b0a
Tyson Romero <romennoodles23@yahoo.com>
2021-09-22 13:38:47 -0600

    dogecoin name and image in DOM

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=nature
         document.body.style.backgroundImage = `url('https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjIwNTV8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzIyNjU1NDU&ixlib=rb-1.2.1&q=85')`
         document.querySelector('#author-name').textContent = "By: Jay Mantri"
     })
+
+fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        document.querySelector('#crypto').innerHTML = `<img src = "${data.image.small}"> <span>${data.name}</span>`
+    })
+    .catch(err => console.error(err))
